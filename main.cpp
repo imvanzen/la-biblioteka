5030e7cb7d3bdbfcf78a51b778f42d7eadba8a6b
Jakub Reczko <jakub.r@masterborn.com>
2024-04-17 20:30:04 +0200

    Add main.cpp file with initial code

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "lib/Book.h"
+
+using namespace std;
+
+int main()
+{
+  cout << "Jakub Reczko - Programowanie obiektowe - Projekt Zaliczeniowy" << endl;
+
+  return 0;
+}
