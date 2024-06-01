82c1f3df516b46f590748419a55beed679e6eb47
Jakub Reczko <jakub.r@masterborn.com>
2024-06-01 19:44:58 +0200

    chore: add Views library and link it to la-biblioteka target

---
+++ src/Views.cpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+#include "Views.h"
+
+UserViews::UserViews()
+{
+}
+BookViews::BookViews()
+{
+}
---
+++ src/Views.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+class UserViews
+{
+};
+
+class BookViews
+{
+};
