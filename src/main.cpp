e90abb0a3df0e297f2b75855fe0cdd20a300b61a
Jakub Reczko <jakub.r@masterborn.com>
2024-06-02 15:15:16 +0200

    chore: Refactor main menu to include user search functionality

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,126 @@ using std::string;
 
 #define Q 'q'
 
+void findBook(Library &library);
+void addBook(Library &library);
+void editBook(Library &library);
+void removeBook(Library &library);
+
+void findUser(Library &library);
+
+/**
+ * Main function
+ *
+ * @return int
+ */
+int main()
+{
+  Storage storage("./dbFile.dat");
+
+  Book book1("Pan Tadeusz", "Adam Mickiewicz", "978-83-288-2376-0", 1834, "Czytelnik", 328);
+  Book book2("Dziady", "Adam Mickiewicz", "978-83-288-2376-0", 1834, "Czytelnik", 328);
+  Book book3("W pustyni i w puszczy", "Henryk Sienkiewicz", "978-83-288-2376-0", 1911, "Czytelnik", 328);
+  Book book4("Krzyżacy", "Henryk Sienkiewicz", "978-83-288-2376-0", 1900, "Czytelnik", 328);
+  Book book5("Ogniem i mieczem", "Henryk Sienkiewicz", "978-83-288-2376-0", 1884, "Czytelnik", 328);
+  Book book6("Quo Vadis", "Henryk Sienkiewicz", "978-83-288-2376-0", 1896, "Czytelnik", 328);
+
+  User user1("Jan Kowalski", "jan.kowalski@mail.com", "ul. Kowalska 1", "123456789");
+  User user2("Anna Nowak", "anna.kowalska@mail.com", "ul. Nowak 1", "987654321");
+  User user3("Piot Kowalski", "piotr.kowalski@mail.com", "ul. Kowalska 2", "123456789");
+  User user4("Kasia Nowak", "kasia.nowak@mail.com", "ul. Nowak 2", "987654321");
+
+  Library library;
+  library.addBook(book1);
+  library.addBook(book2);
+  library.addBook(book3);
+  library.addBook(book4);
+  library.addBook(book5);
+  library.addBook(book6);
+
+  library.addUser(user1);
+  library.addUser(user2);
+  library.addUser(user3);
+  library.addUser(user4);
+
+  clearScreen();
+
+  cout << "Jakub Reczko - Programowanie obiektowe - Projekt Zaliczeniowy" << endl;
+  cout << "La Biblioteca - Menu Głowne" << endl;
+
+  char option;
+  do
+  {
+    /**
+     * Menu
+     */
+    cout << "1. Znajdź książkę" << endl;
+    cout << "2. Dodaj książkę" << endl;
+    cout << "3. Edytuj książkę" << endl;
+    cout << "4. Usuń książkę" << endl;
+    cout << endl;
+    cout << "5. Znajdź czytelnika" << endl;
+    cout << "6. Dodaj czytelnika" << endl;
+    cout << "7. Edytuj czytelnika" << endl;
+    cout << "8. Usuń czytelnika" << endl;
+    cout << endl;
+    cout << "r. Zapis do pliku" << endl;
+    cout << "w. Odczyt z pliku" << endl;
+    cout << Q << ". Wyjście" << endl;
+
+    /**
+     * Get the option
+     */
+    cout << "Wybierz opcje: ";
+    option = getchar();
+
+    /**
+     * Switch statement
+     */
+    switch (option)
+    {
+    case '1':
+      findBook(library);
+      break;
+    case '2':
+      addBook(library);
+      break;
+    case '3':
+      editBook(library);
+      break;
+    case '4':
+      removeBook(library);
+      break;
+    case '5':
+      findUser(library);
+      break;
+    case '6':
+
+      break;
+    case '7':
+
+      break;
+    case '8':
+
+      break;
+    case 'r':
+
+      break;
+    case 'w':
+
+      break;
+    case Q:
+      break;
+    }
+
+    pause();
+
+  } while (option != Q);
+
+  cout << "Do widzenia!" << endl;
+
+  return 0;
+}
+
 /**
  * Find book in library
  *
@@ -181,114 +301,22 @@ void removeBook(Library &library)
 }
 
 /**
- * Main function
+ * Find user in library
  *
- * @return int
  */
-int main()
+void findUser(Library &library)
 {
-  Storage storage("./dbFile.dat");
-
-  Book book1("Pan Tadeusz", "Adam Mickiewicz", "978-83-288-2376-0", 1834, "Czytelnik", 328);
-  Book book2("Dziady", "Adam Mickiewicz", "978-83-288-2376-0", 1834, "Czytelnik", 328);
-  Book book3("W pustyni i w puszczy", "Henryk Sienkiewicz", "978-83-288-2376-0", 1911, "Czytelnik", 328);
-  Book book4("Krzyżacy", "Henryk Sienkiewicz", "978-83-288-2376-0", 1900, "Czytelnik", 328);
-  Book book5("Ogniem i mieczem", "Henryk Sienkiewicz", "978-83-288-2376-0", 1884, "Czytelnik", 328);
-  Book book6("Quo Vadis", "Henryk Sienkiewicz", "978-83-288-2376-0", 1896, "Czytelnik", 328);
-
-  User user1("Jan Kowalski", "jan.kowalski@mail.com", "ul. Kowalska 1", "123456789");
-  User user2("Anna Nowak", "anna.kowalska@mail.com", "ul. Nowak 1", "987654321");
-  User user3("Piot Kowalski", "piotr.kowalski@mail.com", "ul. Kowalska 2", "123456789");
-  User user4("Kasia Nowak", "kasia.nowak@mail.com", "ul. Nowak 2", "987654321");
-
-  Library library;
-  library.addBook(book1);
-  library.addBook(book2);
-  library.addBook(book3);
-  library.addBook(book4);
-  library.addBook(book5);
-  library.addBook(book6);
-
-  library.addUser(user1);
-  library.addUser(user2);
-  library.addUser(user3);
-  library.addUser(user4);
-
-  clearScreen();
-
-  cout << "Jakub Reczko - Programowanie obiektowe - Projekt Zaliczeniowy" << endl;
-  cout << "La Biblioteca - Menu Głowne" << endl;
-
-  char option;
-  do
+  string name;
+  cout << "Znajdź czytelnika" << endl;
+  cout << "Podaj imię i nazwisko czytelnika: ";
+  cin >> name;
+  User *foundUser = library.findUser(name);
+  if (foundUser != nullptr)
   {
-    /**
-     * Menu
-     */
-    cout << "1. Znajdź książkę" << endl;
-    cout << "2. Dodaj książkę" << endl;
-    cout << "3. Edytuj książkę" << endl;
-    cout << "4. Usuń książkę" << endl;
-    cout << endl;
-    cout << "5. Znajdź czytelnika" << endl;
-    cout << "6. Dodaj czytelnika" << endl;
-    cout << "7. Edytuj czytelnika" << endl;
-    cout << "8. Usuń czytelnika" << endl;
-    cout << endl;
-    cout << "r. Zapis do pliku" << endl;
-    cout << "w. Odczyt z pliku" << endl;
-    cout << Q << ". Wyjście" << endl;
-
-    /**
-     * Get the option
-     */
-    cout << "Wybierz opcje: ";
-    option = getchar();
-
-    /**
-     * Switch statement
-     */
-    switch (option)
-    {
-    case '1':
-      findBook(library);
-      break;
-    case '2':
-      addBook(library);
-      break;
-    case '3':
-      editBook(library);
-      break;
-    case '4':
-
-      break;
-    case '5':
-
-      break;
-    case '6':
-
-      break;
-    case '7':
-
-      break;
-    case '8':
-
-      break;
-    case 'r':
-
-      break;
-    case 'w':
-
-      break;
-    case Q:
-      break;
-    }
-
-    pause();
-
-  } while (option != Q);
-
-  cout << "Do widzenia!" << endl;
-
-  return 0;
+    cout << "Znaleziono czytelnika: " << foundUser->getName() << endl;
+  }
+  else
+  {
+    cout << "Nie znaleziono czytelnika" << endl;
+  }
 }
