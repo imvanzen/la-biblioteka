4815197e83ca318672a3adcd60d37a600795cdd2
Jakub Reczko <jakub.r@masterborn.com>
2024-06-02 16:11:18 +0200

    refactor: improve input handling and user experience in main menu

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,9 @@ void editBook(Library &library);
 void removeBook(Library &library);
 
 void findUser(Library &library);
+void addUser(Library &library);
+void editUser(Library &library);
+void removeUser(Library &library);
 
 /**
  * Main function
@@ -147,16 +150,21 @@ void findBook(Library &library)
   string title;
   cout << "Znajdź książkę" << endl;
   cout << "Podaj tytuł książki: ";
+  fflush(stdin);
   getline(cin, title);
   Book *foundBook = library.findBook(title);
-  if (foundBook != nullptr)
-  {
-    cout << "Znaleziono książkę: " << foundBook->getTitle() << endl;
-  }
-  else
+  if (foundBook == nullptr)
   {
     cout << "Nie znaleziono książki" << endl;
+    return;
   }
+
+  cout << "Znaleziono książkę: " << foundBook->getTitle() << endl;
+  cout << "Autor: " << foundBook->getAuthor() << endl;
+  cout << "ISBN: " << foundBook->getIsbn() << endl;
+  cout << "Rok wydania: " << foundBook->getYear() << endl;
+  cout << "Wydawca: " << foundBook->getPublisher() << endl;
+  cout << "Ilość stron: " << foundBook->getPages() << endl;
 }
 
 /**
@@ -208,89 +216,86 @@ void editBook(Library &library)
   getline(cin, title);
   Book *foundBook = library.findBook(title);
 
-  if (foundBook != nullptr)
-  {
-    cout << "Znaleziono książkę: " << foundBook->getTitle() << endl;
-
-    char option;
-
-    do
-    {
-      cout << "Co chcesz edytować?" << endl;
-      cout << "1. Tytuł" << endl;
-      cout << "2. Autor" << endl;
-      cout << "3. ISBN" << endl;
-      cout << "4. Rok wydania" << endl;
-      cout << "5. Wydawca" << endl;
-      cout << "6. Ilość stron" << endl;
-      cout << "b. Powrót" << endl;
-
-      cout << "Wybierz opcje: ";
-      cin >> option;
-      cin.clear();             // Clear the error flags
-      cin.ignore(10000, '\n'); // Ignore the newline character
-
-      string newValueTekst = "";
-      int newValueNumer = 0;
-      fflush(stdin);
-
-      switch (option)
-      {
-      case '1':
-        cout << "Podaj nowy tytuł książki: ";
-        fflush(stdin);
-        getline(cin, newValueTekst);
-        foundBook->setTitle(newValueTekst);
-        cout << "Zmieniono tytuł książki na: " << foundBook->getTitle() << endl;
-        break;
-      case '2':
-        cout << "Podaj nowego autora książki: ";
-        fflush(stdin);
-        getline(cin, newValueTekst);
-        foundBook->setAuthor(newValueTekst);
-        cout << "Zmieniono autora książki na: " << foundBook->getAuthor() << endl;
-        break;
-      case '3':
-        cout << "Podaj nowy ISBN książki: ";
-        fflush(stdin);
-        getline(cin, newValueTekst);
-        foundBook->setIsbn(newValueTekst);
-        cout << "Zmieniono ISBN książki na: " << foundBook->getIsbn() << endl;
-        break;
-      case '4':
-        cout << "Podaj nowy rok wydania książki: ";
-        fflush(stdin);
-        cin >> newValueNumer;
-        foundBook->setYear(newValueNumer);
-        cout << "Zmieniono rok wydania książki na: " << foundBook->getYear() << endl;
-        break;
-      case '5':
-        cout << "Podaj nowego wydawcę książki: ";
-        fflush(stdin);
-        getline(cin, newValueTekst);
-        foundBook->setPublisher(newValueTekst);
-        cout << "Zmieniono wydawcę książki na: " << foundBook->getPublisher() << endl;
-        break;
-      case '6':
-        cout << "Podaj nową ilość stron książki: ";
-        fflush(stdin);
-        cin >> newValueNumer;
-        foundBook->setPages(newValueNumer);
-        cout << "Zmieniono ilość stron książki na: " << foundBook->getPages() << endl;
-        break;
-      case 'b':
-        break;
-      default:
-        cout << "Nieprawidłowa opcja" << endl;
-        break;
-      }
-
-    } while (option != 'b');
-  }
-  else
+  if (foundBook == nullptr)
   {
     cout << "Nie znaleziono książki" << endl;
+    return;
   }
+
+  cout << "Znaleziono książkę: " << foundBook->getTitle() << endl;
+  char option;
+  do
+  {
+    cout << "Co chcesz edytować?" << endl;
+    cout << "1. Tytuł" << endl;
+    cout << "2. Autor" << endl;
+    cout << "3. ISBN" << endl;
+    cout << "4. Rok wydania" << endl;
+    cout << "5. Wydawca" << endl;
+    cout << "6. Ilość stron" << endl;
+    cout << "b. Powrót" << endl;
+
+    cout << "Wybierz opcje: ";
+    cin >> option;
+    cin.clear();             // Clear the error flags
+    cin.ignore(10000, '\n'); // Ignore the newline character
+
+    string newValueTekst = "";
+    int newValueNumer = 0;
+    fflush(stdin);
+
+    switch (option)
+    {
+    case '1':
+      cout << "Podaj nowy tytuł książki: ";
+      fflush(stdin);
+      getline(cin, newValueTekst);
+      foundBook->setTitle(newValueTekst);
+      cout << "Zmieniono tytuł książki na: " << foundBook->getTitle() << endl;
+      break;
+    case '2':
+      cout << "Podaj nowego autora książki: ";
+      fflush(stdin);
+      getline(cin, newValueTekst);
+      foundBook->setAuthor(newValueTekst);
+      cout << "Zmieniono autora książki na: " << foundBook->getAuthor() << endl;
+      break;
+    case '3':
+      cout << "Podaj nowy ISBN książki: ";
+      fflush(stdin);
+      getline(cin, newValueTekst);
+      foundBook->setIsbn(newValueTekst);
+      cout << "Zmieniono ISBN książki na: " << foundBook->getIsbn() << endl;
+      break;
+    case '4':
+      cout << "Podaj nowy rok wydania książki: ";
+      fflush(stdin);
+      cin >> newValueNumer;
+      foundBook->setYear(newValueNumer);
+      cout << "Zmieniono rok wydania książki na: " << foundBook->getYear() << endl;
+      break;
+    case '5':
+      cout << "Podaj nowego wydawcę książki: ";
+      fflush(stdin);
+      getline(cin, newValueTekst);
+      foundBook->setPublisher(newValueTekst);
+      cout << "Zmieniono wydawcę książki na: " << foundBook->getPublisher() << endl;
+      break;
+    case '6':
+      cout << "Podaj nową ilość stron książki: ";
+      fflush(stdin);
+      cin >> newValueNumer;
+      foundBook->setPages(newValueNumer);
+      cout << "Zmieniono ilość stron książki na: " << foundBook->getPages() << endl;
+      break;
+    case 'b':
+      break;
+    default:
+      cout << "Nieprawidłowa opcja" << endl;
+      break;
+    }
+
+  } while (option != 'b');
 }
 
 /**
@@ -304,16 +309,20 @@ void removeBook(Library &library)
   cout << "Podaj tytuł książki: ";
   getline(cin, title);
   Book *foundBook = library.findBook(title);
-  if (foundBook != nullptr)
-  {
-    cout << "Znaleziono książkę: " << foundBook->getTitle() << endl;
-    library.removeBook(*foundBook);
-    cout << "Usunięto książkę" << endl;
-  }
-  else
+  if (foundBook == nullptr)
   {
     cout << "Nie znaleziono książki" << endl;
+    return;
   }
+  cout << "Znaleziono książkę: " << foundBook->getTitle() << endl;
+  cout << "Czy na pewno chcesz usunąć książkę? (t/n): ";
+  char option;
+  fflush(stdin);
+  cin >> option;
+  if (option != 't')
+    return;
+  library.removeBook(*foundBook);
+  cout << "Usunięto książkę" << endl;
 }
 
 /**
@@ -325,14 +334,152 @@ void findUser(Library &library)
   string name;
   cout << "Znajdź czytelnika" << endl;
   cout << "Podaj imię i nazwisko czytelnika: ";
+  fflush(stdin);
   getline(cin, name);
   User *foundUser = library.findUser(name);
-  if (foundUser != nullptr)
-  {
-    cout << "Znaleziono czytelnika: " << foundUser->getName() << endl;
-  }
-  else
+  if (foundUser == nullptr)
   {
     cout << "Nie znaleziono czytelnika" << endl;
+    return;
   }
+
+  cout << "Znaleziono czytelnika: " << foundUser->getName() << endl;
+  cout << "Email: " << foundUser->getEmail() << endl;
+  cout << "Adres: " << foundUser->getAddress() << endl;
+  cout << "Telefon: " << foundUser->getPhoneNumber() << endl;
+}
+
+/**
+ * Add user to library
+ */
+void addUser(Library &library)
+{
+  string name;
+  string email;
+  string address;
+  string phone;
+
+  cout << "Dodaj czytelnika" << endl;
+  cout << "Podaj imię i nazwisko czytelnika: ";
+  fflush(stdin);
+  getline(cin, name);
+  cout << "Podaj email czytelnika: ";
+  fflush(stdin);
+  getline(cin, email);
+  cout << "Podaj adres czytelnika: ";
+  fflush(stdin);
+  getline(cin, address);
+  cout << "Podaj telefon czytelnika: ";
+  fflush(stdin);
+  getline(cin, phone);
+
+  User user(name, email, address, phone);
+  library.addUser(user);
+}
+
+/**
+ * Edit user in library
+ */
+void editUser(Library &library)
+{
+  string name;
+  cout << "Edytuj czytelnika" << endl;
+  cout << "Podaj imię i nazwisko czytelnika: ";
+  fflush(stdin);
+  getline(cin, name);
+  User *foundUser = library.findUser(name);
+
+  if (foundUser == nullptr)
+  {
+    cout << "Nie znaleziono czytelnika" << endl;
+    return
+  };
+
+  cout << "Znaleziono czytelnika: " << foundUser->getName() << endl;
+
+  char option;
+
+  do
+  {
+    cout << "Co chcesz edytować?" << endl;
+    cout << "1. Imię i nazwisko" << endl;
+    cout << "2. Email" << endl;
+    cout << "3. Adres" << endl;
+    cout << "4. Telefon" << endl;
+    cout << "b. Powrót" << endl;
+
+    cout << "Wybierz opcje: ";
+    cin >> option;
+    cin.clear();             // Clear the error flags
+    cin.ignore(10000, '\n'); // Ignore the newline character
+
+    string newValueTekst = "";
+    int newValueNumber = 0;
+    fflush(stdin);
+
+    switch (option)
+    {
+    case '1':
+      cout << "Podaj nowe imię i nazwisko czytelnika: ";
+      fflush(stdin);
+      getline(cin, newValueTekst);
+      foundUser->setName(newValueTekst);
+      cout << "Zmieniono imię i nazwisko czytelnika na: " << foundUser->getName() << endl;
+      break;
+    case '2':
+      cout << "Podaj nowy email czytelnika: ";
+      fflush(stdin);
+      getline(cin, newValueTekst);
+      foundUser->setEmail(newValueTekst);
+      cout << "Zmieniono email czytelnika na: " << foundUser->getEmail() << endl;
+      break;
+    case '3':
+      cout << "Podaj nowy adres czytelnika: ";
+      fflush(stdin);
+      getline(cin, newValueTekst);
+      foundUser->setAddress(newValueTekst);
+      cout << "Zmieniono adres czytelnika na: " << foundUser->getAddress() << endl;
+      break;
+    case '4':
+      cout << "Podaj nowy telefon czytelnika: ";
+      fflush(stdin);
+      getline(cin, newValueTekst);
+      foundUser->setPhoneNumber(newValueTekst);
+      cout << "Zmieniono telefon czytelnika na: " << foundUser->getPhoneNumber() << endl;
+      break;
+    case 'b':
+      break;
+    default:
+      cout << "Nieprawidłowa opcja" << endl;
+      break;
+    }
+  } while (option != 'b');
+}
+
+/**
+ * Remove user from library
+ */
+void removeUser(Library &library)
+{
+  string name;
+  cout << "Usuń czytelnika" << endl;
+  cout << "Podaj imię i nazwisko czytelnika: ";
+  fflush(stdin);
+  getline(cin, name);
+  User *foundUser = library.findUser(name);
+  if (foundUser == nullptr)
+  {
+    cout << "Nie znaleziono czytelnika" << endl;
+    return;
+  }
+
+  cout << "Znaleziono czytelnika: " << foundUser->getName() << endl;
+  cout << "Czy na pewno chcesz usunąć czytelnika? (t/n): ";
+  char option;
+  fflush(stdin);
+  cin >> option;
+  if (option != 't')
+    return;
+  library.removeUser(*foundUser);
+  cout << "Usunięto czytelnika" << endl;
 }
